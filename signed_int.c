9bc8ccd25f8fcb87155bbbfbbce5fe8dbdc3e633
Vani-95 <vanim11502@gmail.com>
2025-06-14 00:06:54 +0530

    Update signed_int.c

--- signed_int.c
+++ signed_int.c
@@ -5,5 +5,6 @@ int main() {
 		long long int max= (long long int) (pow(2,63) -1);
 		long long int min= (long long int) (pow(2,63) * -1);
 	        printf("Highest signed integer number is %lld\n",max);
-		printf("Lowest signed integer number is %lld\n",min);	
+		printf("Lowest signed integer number is %lld\n",min);
+		return 0;
 }
